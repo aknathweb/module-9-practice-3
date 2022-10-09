147dd29d71ee5266cc0d62f8763c9f682fd8f6b9
aknathweb <aniknwu.cse@gmail.com>
2022-10-09 17:32:33 +0600

    crate multiple component and set path

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './Components/Home/Home';
+import Main from './Layout/Main';
+import Orders from './Components/Orders/Orders';
+import About from './Components/About/About';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          element: <Home></Home>
+        },
+        {
+          path: '/home',
+          element: <Home></Home>
+        },
+        {
+          path: '/orders',
+          element: <Orders></Orders>
+        },
+        {
+          path: '/about',
+          element: <About></About>
+        }
+      ]
+    },
+    {
+      path: '*',
+      element: <div><h1>wrong Route Page</h1></div>
+    }
+  ])
   return (
     <div className="App">
-
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
